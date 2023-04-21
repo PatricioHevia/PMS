72f08b6ba4c652cf147ceca9c5129f4c56272ffa
Patricio Hevia <patricio.hevia@ug.uchile.cl>
2023-04-21 17:12:07 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
